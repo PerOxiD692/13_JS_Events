d4fbda39f9c329c149b32a6d60fd72338818dc2f
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-27 14:55:31 +0200

    function togglStatus ++

--- events.js
+++ events.js
@@ -1,14 +1,51 @@
 
-/* Tools */
+
+
+//ausgabe(btn);
+let btn = document.getElementById("trigBtn");
+btn.addEventListener("click",togglStatus);
+
+let status = true;
+
+/***** Änderungen in der View Schicht *****/
+
+//switchClassname("night");
+
+function switchClassname(modeStr) {
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
+
+
+
+
+/***** Tools *****/
+
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
 
-let btn = document.getElementById("trigBtn");
-ausgabe(btn);
-
-btn.addEventListener("click",test);
-
 function test() {
-    ausgabe("hi");
+
+    let cond = (document.body.className == "day");
+    if (cond) {
+        // night-mode
+        switchClassname("night");
+        switchBtnTxt("day");
+    } else {
+        // day-mode
+        switchClassname("day");
+        switchBtnTxt("night");
+    }
+    
+}
+
+function switchBtnTxt(modeStr) {
+    btn.innerHTML = modeStr;
+}
+
+function togglStatus() {
+    status = !status;
+    ausgabe(status);
 }
