8bcfb5967656ce76ff8f055255ec48943203f56c
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-27 13:03:35 +0200

    JS click button ++

--- events.js
+++ events.js
@@ -4,4 +4,11 @@ function ausgabe(outputStr) {
     console.log(outputStr);
 }
 
+let btn = document.getElementById("trigBtn");
+ausgabe(btn);
 
+btn.addEventListener("click",test);
+
+function test() {
+    ausgabe("hi");
+}
